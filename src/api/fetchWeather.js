b136d4c17e39a3da5fc41a614c786403a7356a59
Omkar Arora <aroraomkar@gmail.com>
2021-01-30 16:02:36 +0530

    base app created'

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const URL = process.env.REACT_APP_WEATHER_URL;
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY,
+    },
+  });
+  return data;
+};
